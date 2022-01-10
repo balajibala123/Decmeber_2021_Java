90f60635036cb54c20fbf510aa4b299eace9c2c8
balajibala123 <balajicarriers123@gmail.com>
2022-01-10 11:11:37 +0530

    added git codes to upload in github

--- B18-GUI/Yaro.java
+++ B18-GUI/Yaro.java
@@ -14,3 +14,18 @@ public class Yaro  // created a class called yaro and not extending parent
 	}
 	
 }
+
+
+
+//…or create a new repository on the command line
+//echo "# B18" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/balajibala123/B18.git
+//git push -u origin main
+//…or push an existing repository from the command line
+//git remote add origin https://github.com/balajibala123/B18.git
+//git branch -M main
+//git push -u origin main
